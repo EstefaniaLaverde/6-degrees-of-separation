bf27904173db820d49e93d8fc4aa4bf33a4c00c5
Estefania Laverde <estefania.laverde@urosario.edu.co>
2021-05-12 00:33:33 -0500

    Tomar los datos de la API de twitter

--- Grafo/getData.py
+++ Grafo/getData.py
@@ -16,7 +16,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 #VARIABLES
-userAt = "jaraalejo88"
+userAt = "EstefaniaLaver4"
 limiteDeFollowers = 30
 
 #FUNCTIONS
@@ -30,12 +30,16 @@ def saveJson(data,fileName):
 
     file.close()
 
-def direction(userAt, lista):
+def direction(userAt, lista, t):
     friendToUser = []
-
-    for friend in lista:
-        direction = (userAt, friend)
-        friendToUser.append(direction)
+    if t==1:
+        for friend in lista:
+            direction = (userAt, friend)
+            friendToUser.append(direction)
+    elif t==0:
+        for friend in lista:
+            direction = (friend,userAt)
+            friendToUser.append(direction)
     return friendToUser
 
 def findFollowers(userAt, limite):
@@ -50,7 +54,7 @@ def findFollowers(userAt, limite):
 
 def findAllFollowers(userAt, limiteFollowers, found, limite):
     followers = findFollowers(userAt,limiteFollowers)
-    saveJson(direction(userAt,followers), "dataFollowers")
+    saveJson(direction(userAt,followers,1), "dataFollowers.txt")
     found.append(userAt)
 
     for i in range(0,limite):
@@ -59,7 +63,7 @@ def findAllFollowers(userAt, limiteFollowers, found, limite):
             if userAt not in found:
                 followers += findFollowers(userAt,limiteFollowers)
                 found.append(userAt)
-                saveJson(direction(userAt,findFollowers(userAt,limiteFollowers)), "dataFollowers")
+                saveJson(direction(userAt,findFollowers(userAt,limiteFollowers),1), "dataFollowers.txt")
                 followers.remove(userAt)
             else:
                 continue
@@ -68,7 +72,45 @@ def findAllFollowers(userAt, limiteFollowers, found, limite):
             continue
 
 # findAllFollowers(userAt,limiteDeFollowers,7)
-found=['marinapistacho','EstefaniaLaver4','auro_querol','BeaaM_','mnadalcalvo','CortizoKevin','PaNdA_Df','LaInvisible04','sebas7243','andreagomezjai1','Oscar_juli70','santiagozuiga7']
-found += ['RorroMC01','QuincyCoghill','Posue_Ronis','Gabriela_Musica','MeliValenez','jcastronauta','chuckygarcia','Toms90257478','SofaRubiano3','jaraalejo88','Juanpa15higuera','pau13_andrea','Saardiaz','Sofia_Lopez_07']
-findAllFollowers(userAt,limiteDeFollowers, found, 7)
+# found=['marinapistacho','EstefaniaLaver4','auro_querol','BeaaM_','mnadalcalvo','CortizoKevin','PaNdA_Df','LaInvisible04','sebas7243','andreagomezjai1','Oscar_juli70','santiagozuiga7']
+# found += ['RorroMC01','QuincyCoghill','Posue_Ronis','Gabriela_Musica','MeliValenez','jcastronauta','chuckygarcia','Toms90257478','SofaRubiano3','jaraalejo88','Juanpa15higuera','pau13_andrea','Saardiaz','Sofia_Lopez_07']
+# findAllFollowers(userAt,limiteDeFollowers, found, 7)
 
+def findFriends(userAt, limite):
+    #Encuentra los seguidos por el usuario. En terminos de twitter los amigos.
+    friendsFollowingAt = [] #@ de amigos
+
+    for user in tweepy.Cursor(api.friends, screen_name=userAt).items(limite):
+        friend = json.loads(json.dumps(user._json, indent=2))
+        friendsFollowingAt.append(friend['screen_name'])
+
+    return friendsFollowingAt
+# findFriends(userAt,20)
+def findAllFriends(userAt, limiteFriends, found, limite):
+    friends = findFriends(userAt,limiteFriends)
+    # saveJson(direction(userAt,friends,0), "dataFriends")
+    # found.append(userAt)
+
+    # for i in range(0,limite):
+    #     try:
+    #         userAt = friends[i]
+    #         if userAt not in found:
+    #             friends += findFriends(userAt,limiteFriends)
+    #             found.append(userAt)
+    #             saveJson(direction(userAt,findFollowers(userAt,limiteFriends),0), "dataFriends.txt")
+    #             friends.remove(userAt)
+    #         else:
+    #             continue
+    #     except tweepy.RateLimitError:
+    #         time.sleep(60 * 15)
+    #         continue
+    # for user in found:
+    #     try:
+    #         userAt=user
+    #         if userAt not in aux:
+    #             friends += findFriends(userAt,limiteFriends)
+    #             found.append(userAt)
+    #             saveJson(direction(userAt,findFollowers(userAt,limiteFriends),0), "dataFriends.txt")
+    #             friends.remove(userAt)
+found = []
+findAllFriends(userAt,limiteDeFollowers, found, 7)
