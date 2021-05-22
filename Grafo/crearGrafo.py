9455872c0be3ce8651659b234cdd7292151c5b2a
sebastianssg <56741758+sebastianssg@users.noreply.github.com>
2021-05-22 17:33:48 -0500

    Update crearGrafo.py

--- Grafo/crearGrafo.py
+++ Grafo/crearGrafo.py
@@ -80,32 +80,41 @@ def createGraph(listaDirecciones):
         edges.append((indexNodo2,indexNodo1))
 
 
-    graph = ig.Graph(n=len(nodes), vertex_attrs = {'name':nodes}, directed= True, edges = edges)
+    graph = ig.Graph(n=len(nodes), vertex_attrs = {'name':nodes}, directed = True, edges = edges)
     graph.es["weight"] = 1.0
 
     return graph, nodes
 
 
-
-
 #ll = [('pepe','yo'),('yo','tu'),('ji','jo'),('yo','pepa'),('pepa','yo'),('pepa','pepita')] #REVISAR DIRECCIONES
 gr , visited = createGraph(direcciones)
+g_dir , visited = createGraph(direcciones)
 #print(gr.es[0].attributes())
 
 gr.to_undirected(mode='collapse', combine_edges=None)
 
 #print(gr)
 
-"""         layouts           """
+def SeisGrados():
+    origen=input("Origen: ")
+    destino=input("Destino: ")
+    camino = gr.shortest_paths(source=origen, target=destino, mode='out')
+    return print("La longitud del camino es de:", camino[0][0])
+
+SeisGrados()
+
+"""------------------------ layouts ---------------------------"""
 #layout = gr.layout('grid_fr')
 layout= gr.layout_davidson_harel()
 #layout= gr.layout_graphopt()
 #layout= gr.layout_lgl()
 #layout= gr.layout_reingold_tilford_circular()
 #layout= gr.layout_sugiyama()
-"""         plot           """
-ig.plot(gr, layout = layout,target='myfile.png',bbox = (4000, 4000), margin = 20,vertex_label=visited)
-"""         numeros           """
+
+"""------------------------- plot --------------------------"""
+ig.plot(gr, layout = layout,target='grafo.png',bbox = (4000, 4000), margin = 20,vertex_label=visited)
+ig.plot(g_dir, layout = layout,target='digrafo.png',bbox = (4000, 4000), margin = 20,vertex_label=visited)
+"""------------------------- Numeros --------------------------"""
 N_V = gr.vcount()
 #N_E = gr.ecount()
 
@@ -124,23 +133,33 @@ for path in Corto1:
 a=0
 for path in Corto1:
     a+=sum(path)
+
+
+suma1=0
+suma2=0
+for path in Corto1:
+    suma1 += sum(path)
+    suma2 += len(path)
+    prom = suma1/suma2
+
 print("Indice de Wiener",a)
-
-
-#print("Maximo: ", max(Corto1[0]))
-
-
-print("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
-print("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
-print("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
+#print(Corto1)
+print("Promedio shortest_paths: ", prom )
 
 print("Eccentricity: ",gr.eccentricity(vertices=None, mode='all'))
+print("Min EEE: ", min(gr.eccentricity(vertices=None, mode='all')))
+print("Max EEE: ", max(gr.eccentricity(vertices=None, mode='all')))
 print("Radio: ", gr.radius(mode='all'))
 print("Diametro: ", gr.diameter(directed=False, unconn=True, weights=None))
 print("Get_Diametro: ", gr.get_diameter(directed=False, unconn=True, weights=None))
 print("Cintura_F: ", gr.girth(return_shortest_circle=False))
 print("Cintura_T: ", gr.girth(return_shortest_circle=True))
 print("Distancia Promedio: ", a/(comb(N_V,2)))
+
+
+"""--------------------------------------------------------"""
+"""-------------------------TESTS--------------------------"""
+"""--------------------------------------------------------"""
 #print(len(Corto1[0]))
 
 #Corto2 = gr.get_shortest_paths(v='sebas7243', to=None,mode='out',output='vpath')
@@ -154,7 +173,7 @@ print("Distancia Promedio: ", a/(comb(N_V,2)))
 #print(visited)
 #print(gr.vs[31])
 
-#===TESTS===
+
 
 #ig.plot(gr, layout = layout,target='myfile.png',bbox = (2000, 2000),
 #        margin = 20, vertex_color = "green",edge_width=2,
